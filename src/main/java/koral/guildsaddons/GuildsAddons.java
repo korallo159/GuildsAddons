9c4e01968361bcf76582fdfa38e01b9265efc835
koral <orszulak.k@o2.pl>
2021-01-25 14:10:51 +0100

    Initial commit

---
+++ src/main/java/koral/guildsaddons/GuildsAddons.java
@@ -0,0 +1,22 @@
+package koral.guildsaddons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GuildsAddons extends JavaPlugin {
+//TODO:Random TP pomiedzy serwerami i dostepnymi kordami
+//TODO: COBBLEX - Z 9 stakow cobbla w craftingu, dostajesz cobbleX, jak postawisz to wylosowuje jakis losowy item z configa
+//TODO: STONIARKI - Nad specjalnie zcraftowanym itemem respi sie stone
+//TODO: DROP ZE STONE - Wylaczony drop z zwyklych rud, zamiast tego szanse na to ze wydropia z zwyklego stone surowce.
+//TODO
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
