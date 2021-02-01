f575b9d9de5f8846352dbf5503f889da16ef69d7
koral <orszulak.k@o2.pl>
2021-02-01 17:43:22 +0100

    statements

--- src/main/java/koral/guildsaddons/database/statements/PlayersStatements.java
+++ src/main/java/koral/guildsaddons/database/statements/PlayersStatements.java
@@ -1,9 +1,11 @@
 package koral.guildsaddons.database.statements;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static koral.guildsaddons.database.DatabaseConnection.hikari;
@@ -40,6 +42,78 @@ public class PlayersStatements {
                     statement.close();
                 } catch (SQLException e) {
                     e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void updatePlayerData(Player player){
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try{
+            connection = hikari.getConnection();
+            statement = connection.prepareStatement("SELECT * FROM Players WHERE NICK=?");
+            statement.setString(1, player.getName());
+            String update = "UPDATE Players SET playerdata=? WHERE NICK=?";
+            statement = connection.prepareStatement(update);
+
+            statement.setString(1, "string");
+            statement.setString(2, player.getName());
+            statement.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void getMysqlPlayerData(Player player){
+        Connection connection = null;
+        PreparedStatement statement = null;
+        String sql = "SELECT playerdata FROM Players WHERE NICK=?";
+        try{
+            connection = hikari.getConnection();
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, player.getName());
+            ResultSet resultSet = statement.executeQuery();
+       //     while(resultSet.next()){
+                resultSet.next();
+                String data = resultSet.getString("playerdata");
+                if(data != null) {
+
+                }
+     //       }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            if(connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+            if(statement != null){
+                try {
+                    statement.close();
+                } catch (SQLException e2) {
+                    e2.printStackTrace();
                 }
             }
         }
